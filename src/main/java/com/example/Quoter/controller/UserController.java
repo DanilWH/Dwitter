3705a8549d94b7d2a69f2ce6be903b0f941ff8af
DanilToLinus <danil-lomakin-02@mail.ru>
2021-01-03 15:43:17 +0500

    Spring Boot FlyWay: Databases migrations, the user profile. V2 [ADDED] a new migration where admin has been added in the database.\n [FEATURE] The user profile section. Now, the user can change his/her password or email.

--- src/main/java/com/example/Quoter/controller/UserController.java
+++ src/main/java/com/example/Quoter/controller/UserController.java
@@ -1,12 +1,14 @@
 package com.example.Quoter.controller;
 
-import java.util.Arrays;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
+
+import com.example.Quoter.domain.Role;
+import com.example.Quoter.domain.User;
+import com.example.Quoter.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,60 +17,62 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.example.Quoter.domain.Role;
-import com.example.Quoter.domain.User;
-import com.example.Quoter.repos.UserRepo;
-
 @Controller
 @RequestMapping("/user")
-@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
     
     @Autowired
-    private UserRepo userRepo;
+    private UserService userService;
     
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping
     public String userList(Model model) {
-        model.addAttribute("users", userRepo.findAll());
+        model.addAttribute("users", userService.findAll());
         
         return "userList";
     }
     
-    @GetMapping("{userId}")
-    public String userEditForm(@PathVariable(value="userId") Long userId, Model model) {
-        User user = this.userRepo.findById(userId).get();
-        
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @GetMapping("{user}")
+    public String userEditForm(@PathVariable User user, Model model) {
         model.addAttribute("user", user);
         model.addAttribute("roles", Role.values());
         
         return "userEdit";
     }
     
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping
     public String userSave(
             @RequestParam String username,
             @RequestParam Map<String, String> form,
             @RequestParam Long userId
     ) {
-        User user = this.userRepo.findById(userId).get();
-        
-        user.setUsername(username);
-        
-        Set<String> roles = Arrays.stream(Role.values())
-                .map(Role::name)
-                .collect(Collectors.toSet());
-        
-        user.getRoles().clear();
-        
-        for (String key : form.keySet()) {
-            if (roles.contains(key)) {
-                user.getRoles().add(Role.valueOf(key));
-            }
-        }
-        
-        this.userRepo.save(user);
-        
+        this.userService.saveUser(userId, username, form);
+
         return "redirect:/user";
+    }
+
+    @GetMapping("profile")
+    public String getProfile(
+        @AuthenticationPrincipal User user,
+        Model model
+    ) {
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+        
+        return "profile";
+    }
+
+    @PostMapping("profile")
+    public String updateProfile(
+        @AuthenticationPrincipal User user,
+        @RequestParam String password,
+        @RequestParam String email
+    ) {
+        this.userService.updateProfile(user, password, email);
+
+        return "redirect:/user/profile";
     }
 
 }
--- src/main/java/com/example/Quoter/service/UserService.java
+++ src/main/java/com/example/Quoter/service/UserService.java
@@ -1,17 +1,23 @@
 package com.example.Quoter.service;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
 import com.example.Quoter.domain.Role;
 import com.example.Quoter.domain.User;
 import com.example.Quoter.repos.UserRepo;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
-
-import java.util.Collections;
-import java.util.UUID;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -41,17 +47,7 @@ public class UserService implements UserDetailsService {
 
         this.userRepo.save(user);
 
-        if (!StringUtils.isEmpty(user.getEmail())) {
-            String message = String.format(
-                    "Hello, %s \n" +
-                            "Welcome to Quoter. Please, visit the next link: http://localhost:8080/activate/%s",
-                    user.getUsername(),
-                    user.getActivationCode()
-            );
-
-            mailSender.send(user.getEmail(), "Activation code", message);
-        }
-
+        this.sendMessage(user);
         return true;
     }
 
@@ -67,5 +63,69 @@ public class UserService implements UserDetailsService {
         this.userRepo.save(user);
 
         return true;
+    }
+
+    public List<User> findAll() {
+        return this.userRepo.findAll();
+    }
+
+    public void saveUser(Long userId, String username, Map<String, String> form) {
+        User user = this.userRepo.findById(userId).get();
+        
+        user.setUsername(username);
+        
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+        
+        user.getRoles().clear();
+        
+        for (String key : form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+        
+        this.userRepo.save(user);
+    }
+
+    public void updateProfile(User user, String password, String email) {
+        String userEmail = user.getEmail();
+
+        boolean isEmailChanged = (email != null && !email.equals(userEmail)) ||
+                                 (userEmail != null && !userEmail.equals(email));
+
+        if (isEmailChanged) {
+            user.setEmail(email);
+            
+            // set a new activation code if the user's new email isn't empty.
+            if (!StringUtils.isEmpty(email)) {
+                user.setActivationCode(UUID.randomUUID().toString());
+            }
+        }
+
+        if (!StringUtils.isEmpty(password)) {
+            user.setPassword(password);
+        }
+
+        this.userRepo.save(user);
+
+        // send the new activation code by email if the user's new email isn't empty.
+        if (isEmailChanged) {
+            this.sendMessage(user);
+        }
+    }
+
+    private void sendMessage(User user) {
+        if (!StringUtils.isEmpty(user.getEmail())) {
+            String message = String.format(
+                    "Hello, %s \n" +
+                            "Welcome to Quoter. Please, visit the next link: http://localhost:8080/activate/%s",
+                    user.getUsername(),
+                    user.getActivationCode()
+            );
+
+            mailSender.send(user.getEmail(), "Activation code", message);
+        }
     }
 }
